dd7389e6b4a940a49f069cd06c8db2d2640cb86d
dekanovaa <dekanovasevinchhh@gmail.com>
2024-07-03 12:39:56 +0500

    responsivlik qo'shildi

---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,89 @@
+import './Modal.css'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+function Modal() {
+    const [state, setState] = React.useState({
+        right: false,
+      });
+    
+      const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+          return;
+        }
+    
+        setState({ ...state, [anchor]: open });
+      };
+    
+      const list = (anchor) => (
+        <Box
+          sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+          role="presentation"
+          onClick={toggleDrawer(anchor, false)}
+          onKeyDown={toggleDrawer(anchor, false)}
+        >
+          <List>
+            {[
+              <div className='modal__wrap'>
+                <a href='#header' className='modal__link'>Home</a>,
+              <a href='#portfolio' className='modal__link'>Portfolio</a>,
+              <a href='#resume' className='modal__link'>Resume</a>,
+              <a href='#service' className='modal__link'>Service</a>,
+              <a href='#contact' className='modal__link'>Contact</a>
+              </div>
+            ].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+
+        </Box>
+      );
+
+    return(
+        <>
+         {[ 'right'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}><i class="fa-solid fa-bars"></i></Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+        </>
+    )
+}
+export default Modal
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 
+import Modal from '../Modal/Modal'
 import './Navbar.css'
 
 function Navbar() {
  
 
   return (
-    <div className='navbar'>
+     <div className='navbar'>
       <div className='container navbar__container'>
-        <a href='#' className='navbar__link_logo'>Sevinch</a>
+       <div className='navbar__wrap'>
+       <a href='#' className='navbar__link_logo'>Sevinch</a>
         <nav className='navbar__nav'>
           <a href='#header' className='navbar__link'>Home</a>
           <a href='#portfolio' className='navbar__link'>Portfolio</a>
@@ -15,6 +17,8 @@ function Navbar() {
           <a href='#service' className='navbar__link'>Service</a>
           <a href='#contact' className='navbar__link'>Contact</a>
         </nav>
+       </div>
+        <Modal/>
       </div>
     </div>
   )
